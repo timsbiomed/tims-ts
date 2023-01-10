5b5c07e44e11e4ee6c17e24bd0abad94aec06fbe
Shahim Essaid <shahim@essaid.com>
2023-01-10 11:21:25 -0800

    Scripting fixes.

--- bin/.setup-servers-init.sh
+++ bin/.setup-servers-init.sh
@@ -18,19 +18,20 @@ DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 TTS_HOME=$(realpath "${DIR}/..")
 cd "$TTS_HOME"
 if [[ ! -f bin/env.sh ]]; then
-  cp bin/env.sh.example  bin/env.sh
+  cp bin/env.sh.example bin/env.sh
 fi
 . bin/env.sh
 
-if [[ -z ${TTS_VENV:-} || ! -d ${TTS_VENV}  ]]; then
-  python3  -m venv "${TTS_VENV:-.venv}"
-  echo >> bin/env.sh
-  echo "TTS_VENV=${TTS_VENV:-.venv}" >> bin/env.sh
-  . bin/env.sh
+if [[ -z ${TTS_VENV:-} || ! -d ${TTS_VENV} ]]; then
+  python3 -m venv "${TTS_VENV:-.venv}"
+  if [[ -z ${TTS_VENV:-} ]]; then
+    echo >>bin/env.sh
+    echo "TTS_VENV=${TTS_VENV:-.venv}" >>bin/env.sh
+    . bin/env.sh
+  fi
 fi
 
 . "${TTS_VENV}/bin/activate"
 pip -q install -U pip
 pip -q -q uninstall -y setup-servers yq
-pip -q install setup-servers==0.1.13 yq &> /dev/null
-
+pip -q install setup-servers==0.1.13 yq &>/dev/null
